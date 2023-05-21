ad06eafc6e1e8f0cee1845bd390ca15bbda7981e
Arash Baheri <arashbaheri@yahoo.com>
2023-05-21 14:05:11 +0000

    modify some configs and add Docker file and docker-compose file

--- shortner/shortner/settings.py
+++ shortner/shortner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-=yw%qg@fa0-6c^7148j@9oa+7nyhu%fy9dyxh4x^ck$$r9-t1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "shortner.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mariadb",
+        "USER": "mariadb",
+        "PASSWORD": "mariadb",
+        "HOST": "db",
+        "PORT": 3306
     }
 }
 
